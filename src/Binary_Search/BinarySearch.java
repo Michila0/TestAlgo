f51c0c220286256e4054787cebc6635365e87230
Michila <michila.bimsara12@gmail.com>
2023-04-30 23:28:50 +0530

    Creat the function in BinarySearch

--- src/Binary_Search/BinarySearch.java
+++ src/Binary_Search/BinarySearch.java
@@ -1,8 +1,24 @@
 package Binary_Search;
 
 public class BinarySearch {
+    public  static  void binarySearch(int[] arr, int first, int last,int key){
+        int mid = (first+last)/2;
+        while (first <= last){
+            if (arr[mid] <= key){
+                first = mid+1;
+            } else if (arr[mid] == key) {
+                System.out.println("Element is found at index: "+mid);
+                break;
+            }else {
+                last = mid -1;
+            }
+            mid = (first+last)/2;
+        }
+    }
     public static void main(String[] args) {
-        //Array has sorted array
+        int[] arr= {2,5,8,12,16,23,38,56,72,91};//Binary Search array has sorted array
+        int key = 56;
+        binarySearch(arr,0, arr.length-1,key );
     }
 }
 
